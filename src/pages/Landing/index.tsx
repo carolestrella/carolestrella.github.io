d0b0a4ff7c4479429fc27b1d9fcb123d1344c8af
carolestrella <carolina@host.com.br>
2021-03-24 11:12:15 -0300

    first commit

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import CodeImg from '../../assets/images/code.svg'
+import './styles.css'
+
+function Landing() {
+  return(
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+        <div className="logo-container">
+          <img src={CodeImg} alt="code"/>
+        </div>
+
+        <div className="menu-container">
+          <a href="https://carolestrella.github.io/my-page/" className="code">
+            CODE
+          </a>
+          <a href="https://carolestrella.github.io/my-page/" className="art">
+            ART
+          </a>
+          <a href="https://carolestrella.github.io/my-page/" className="about"> 
+            ABOUT
+          </a>
+        </div>
+
+        <div className="switch">
+          <input type="checkbox" id="switch" />
+          <label htmlFor="switch">Toggle</label>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Landing
