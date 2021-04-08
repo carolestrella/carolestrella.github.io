f4d2196b6c874ce7ceb69621b61f4c5e5e8e3347
Carolina Machado <carolinamachado@BRSPPM-FVFF52KTQ6L4.local>
2021-04-07 21:18:38 -0300

    add CodePage

---
+++ src/pages/CodePage/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+import './styles.css'
+
+function CodePage() {
+    return(
+        <div id="code-page">
+            <h1>Code</h1>
+        </div>
+    )
+}
+
+export default CodePage
